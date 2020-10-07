4a8c9cfe077e4b2ced3f405d8288efeb942e44eb
nettee <nettee.liu@gmail.com>
2020-10-07 22:56:12 +0800

    引入 checked exception

--- src/main/java/me/nettee/markdown/parser/SimpleMarkdownParser.java
+++ src/main/java/me/nettee/markdown/parser/SimpleMarkdownParser.java
@@ -7,6 +7,7 @@ import me.nettee.markdown.dom.Image;
 import me.nettee.markdown.dom.ImageParagraph;
 import me.nettee.markdown.dom.Line;
 import me.nettee.markdown.dom.MarkdownDocument;
+import me.nettee.markdown.dom.MarkdownElement;
 import me.nettee.markdown.dom.MathBlock;
 import me.nettee.markdown.dom.NormalParagraph;
 import me.nettee.markdown.dom.Paragraph;
@@ -17,7 +18,6 @@ import me.nettee.markdown.model.FallBack;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 import java.util.function.Predicate;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -26,6 +26,21 @@ import static com.google.common.base.Preconditions.checkState;
 import static java.util.stream.Collectors.toList;
 
 public class SimpleMarkdownParser implements MarkdownParser {
+
+    private static class ParseElementFailException extends Exception {
+
+        private final Line line;
+        private final Class<? extends MarkdownElement> targetClass;
+
+        public ParseElementFailException(Line line, Class<? extends MarkdownElement> targetClass) {
+            this.line = line;
+            this.targetClass = targetClass;
+        }
+
+        public MarkdownParseError toMarkdownParseError() {
+            return new MarkdownParseError(line, targetClass);
+        }
+    }
 
     private final Line[] lines;
     private int pos;
@@ -165,9 +180,11 @@ public class SimpleMarkdownParser implements MarkdownParser {
         List<Line> lines = consumeUntil(Line::isEmpty);
         // 首先尝试 parse 成图片
         if (lines.size() == 1) {
-            Optional<Image> image = tryParseImageFromLine(lines.get(0));
-            if (image.isPresent()) {
-                return FallBack.primary(new ImageParagraph(image.get()));
+            try {
+                Image image = tryParseImageFromLine(lines.get(0));
+                return FallBack.primary(new ImageParagraph(image));
+            } catch (ParseElementFailException e) {
+                // do nothing
             }
         }
         // 如果 parse 失败则作为普通段落处理
@@ -177,39 +194,44 @@ public class SimpleMarkdownParser implements MarkdownParser {
 
     ImageParagraph parseImageParagraph() {
         Line line = nextLine();
-        Optional<Image> image = tryParseImageFromLine(line);
-        checkParserState(image.isPresent(), new MarkdownParseError(line, ImageParagraph.class));
-        return new ImageParagraph(image.get());
+        try {
+            Image image = tryParseImageFromLine(line);
+            return new ImageParagraph(image);
+        } catch (ParseElementFailException e) {
+            throw e.toMarkdownParseError();
+        }
     }
 
-    private static Optional<Image> tryParseImageFromLine(Line line) {
+    private static Image tryParseImageFromLine(Line line) throws ParseElementFailException {
         Pattern pattern = Pattern.compile("^!\\[(.*?)]\\((.+?)\\)$");
         Matcher matcher = pattern.matcher(line.getText());
         boolean found = matcher.find();
         if (!found) {
-            return Optional.empty();
+            throw new ParseElementFailException(line, Image.class);
         }
         String caption = matcher.group(1);
         String uri = matcher.group(2);
-        return Optional.of(new Image(caption, uri));
+        return new Image(caption, uri);
     }
 
     private FallBack<Paragraph, Table, NormalParagraph> tryParseTable() {
         checkState(nextLine().seemsLikeTableBorder());
         List<Line> lines = consumeUntil(Line::isEmpty);
         // 首先尝试 parse 成表格
-        Optional<Table> table = tryParseTableFromLines(lines);
-        if (table.isPresent()) {
-            return FallBack.primary(table.get());
+        try {
+            Table table = tryParseTableFromLines(lines);
+            return FallBack.primary(table);
+        } catch (ParseElementFailException e) {
+            // do nothing
         }
         // 如果 parse 失败则作为普通段落处理
         NormalParagraph normalParagraph = parseNormalParagraphFromLines(lines);
         return FallBack.secondary(normalParagraph);
     }
 
-    private static Optional<Table> tryParseTableFromLines(List<Line> lines) {
-        // TODO;
-        return Optional.empty();
+    private static Table tryParseTableFromLines(List<Line> lines) throws ParseElementFailException {
+        // TODO
+        throw new ParseElementFailException(lines.get(0), Table.class);
     }
 
     NormalParagraph parseNormalParagraph() {
